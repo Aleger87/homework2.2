4adf13150f43b32bbd99e89f6013e6fdb510fa9f
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2022-09-30 00:54:41 +0300

    task1.2

--- src/Human.java
+++ src/Human.java
@@ -3,8 +3,15 @@ public class Human {
     private int birthDate;
     private String name;
     private String currentCity;
+    private String position;
 
+    public void setPosition(String position) {
+        this.position = position;
+    }
 
+    public String getPosition() {
+        return position;
+    }
 
     public String getName() {
         return name;
--- src/Main.java
+++ src/Main.java
@@ -11,33 +11,55 @@ public class Main {
         * */
         LocalDate localDate = LocalDate.now();
 
-        LocalDate birthDay = LocalDate.of(1987, 4, 12);
+        LocalDate birthDayNax = LocalDate.of(1987, 4, 12);
         Human humanMax = new Human();
         humanMax.setName("Максим");
         humanMax.setCurrentCity("Минск");
-        humanMax.setBirthDate(localDate.getYear() - birthDay.getYear());
+        humanMax.setBirthDate(localDate.getYear() - birthDayNax.getYear());
+        humanMax.setPosition("бренд-менеджером");
         System.out.println(humanMax.getName() + " из города " + humanMax.getCurrentCity() + ", которому " +humanMax.getBirthDate() + " лет");
 
-        birthDay = LocalDate.of(1993, 4, 12);
+
+        LocalDate birthDayAnya = LocalDate.of(1993, 4, 12);
         Human humanAnya = new Human();
         humanAnya.setName("Аня");
         humanAnya.setCurrentCity("Москва");
-        humanAnya.setBirthDate(localDate.getYear() - birthDay.getYear());
+        humanAnya.setBirthDate(localDate.getYear() - birthDayAnya.getYear());
+        humanAnya.setPosition("методистом образовательных программ");
         System.out.println(humanAnya.getName() + " из города " + humanAnya.getCurrentCity() + ", которой " +humanAnya.getBirthDate() + " лет");
 
-        birthDay = LocalDate.of(1994, 4, 12);
+        LocalDate birthDayKatya = LocalDate.of(1994, 4, 12);
         Human humanKatya = new Human();
         humanKatya.setName("Катя");
         humanKatya.setCurrentCity("Калининград");
-        humanKatya.setBirthDate(localDate.getYear() - birthDay.getYear());
+        humanKatya.setBirthDate(localDate.getYear() - birthDayKatya.getYear());
+        humanKatya.setPosition("продакт-менеджером");
         System.out.println(humanKatya.getName() + " из города " + humanKatya.getCurrentCity() + ", которой " +humanKatya.getBirthDate() + " лет");
 
-        birthDay = LocalDate.of(1995, 4, 12);
+        LocalDate birthDayArtem = LocalDate.of(1995, 4, 12);
         Human humanArtem = new Human();
         humanArtem.setName("Артем");
         humanArtem.setCurrentCity("Москва");
-        humanArtem.setBirthDate(localDate.getYear() - birthDay.getYear());
+        humanArtem.setBirthDate(localDate.getYear() - birthDayArtem.getYear());
+        humanArtem.setPosition("директором по развитию бизнеса");
         System.out.println(humanArtem.getName() + " из города " + humanArtem.getCurrentCity() + ", которому " +humanArtem.getBirthDate() + " лет");
+
+        System.out.println("Привет! Меня зовут "+humanMax.getName()+
+                            "\nЯ из города " +humanMax.getCurrentCity() +
+                            "\nЯ родился в "+ birthDayNax.getYear()+" году. " +
+                            "\nЯ работаю на должности "+humanMax.getPosition()+" Будем знакомы!\n");
+        System.out.println("Привет! Меня зовут "+humanAnya.getName()+
+                "\nЯ из города " +humanAnya.getCurrentCity() +
+                "\nЯ родился в "+ birthDayAnya.getYear()+" году. " +
+                "\nЯ работаю на должности "+humanAnya.getPosition()+" Будем знакомы!\n");
+        System.out.println("Привет! Меня зовут "+humanKatya.getName()+
+                "\nЯ из города " +humanKatya.getCurrentCity() +
+                "\nЯ родился в "+ birthDayKatya.getYear()+" году. " +
+                "\nЯ работаю на должности "+humanKatya.getPosition()+" Будем знакомы!\n");
+        System.out.println("Привет! Меня зовут "+humanArtem.getName()+
+                "\nЯ из города " +humanArtem.getCurrentCity() +
+                "\nЯ родился в "+ birthDayArtem.getYear()+" году. " +
+                "\nЯ работаю на должности "+humanArtem.getPosition()+" Будем знакомы!\n");
 
     }
 }
